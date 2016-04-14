991282e08feeb3cc4d8c07c7439e33ae375afbe9
Student <matej545@hotmail.com>
2016-04-14 16:29:51 +0200

    7. zadatak komit

--- lv1/lv1/lv1.cpp
+++ lv1/lv1/lv1.cpp
@@ -6,7 +6,11 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+<<<<<<< HEAD
 	printf("Pozdrav iz aplikacije od Matej Umiljanovic, broj indeksa: 4201337");
+=======
+	printf("Pozdrav iz aplikacije od Matej Umiljanovic");
+>>>>>>> 994ccd5... 7. zadatak komit
 	getchar();
 	return 0;
 }
